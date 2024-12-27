dd9ecfbaaf733f7e76e4a2333decf77b907ba418
Thanaphat <thanapat30769@gmail.com>
2024-12-27 19:21:29 +0700

    lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,35 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0;
+    double sum2 = 0;
+    double sum3 = 1;
+    double sum4 = 0;
+    double max = A[0];
+    double min = A[0];
+    for(int i = 0; i < N; i++){
+        sum += A[i];
+    }
+    B[0] = sum/N;
+    for(int j = 0; j < N; j++){
+        sum2 = sum2+pow((A[j]-B[0]),2);
+    }
+    B[1] = sqrt(sum2/N);
+    for(int k = 0; k < N; k++){
+        sum3 = sum3*A[k];
+    }
+    B[2] = pow(sum3,(1.0/N));
+    for(int l = 0; l < N; l++){
+        sum4 += 1.0/A[l];
+    }
+    B[3] = N/sum4;
+    for(int m = 0; m < N; m++){
+        if(A[m]>max) max = A[m];
+        if(A[m]<min) min = A[m];
+    }
+    B[4] = max;
+    B[5] = min;
+    
+}
